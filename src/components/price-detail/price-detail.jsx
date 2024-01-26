d5db98e00a2f84c9963824873318f61705b7ae0d
isaacmayela <isaacmayela27@gmail.com>
2024-01-26 09:30:32 +0100

    feat: adding invoice detail interface

--- src/components/price-detail/price-detail.jsx
+++ src/components/price-detail/price-detail.jsx
@@ -1,11 +1,27 @@
 import "./price-detail.css"
+import PriceComponenent from "../price-component/price-component"
 
 function PriceDetails() {
+
+    const list = [1,2,3,4,5]
 
     return (
       <>
         <div className="price-detail">
-            
+            <div className="price-detail-header">
+                <PriceComponenent name={"Items Name"} quantity={"Quantity"} price={"Price"} total={"Total"}/>
+            </div>
+            <div className="price-container">
+                {
+                    list.map((priceInfo,index) =>(
+                        <PriceComponenent name={"Banner Design"} quantity={"1"} price={"$185"} total={"$225"} style={"strong"} key={index}/>
+                    ))
+                }
+            </div>
+        </div>
+        <div className="total">
+            <p>Total Due</p>
+            <h2>$556</h2>
         </div>
       </>
     )
